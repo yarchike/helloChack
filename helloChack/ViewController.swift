573d11c800b7766541603625dfddca575718bb2a
yarchike <yarchike@gmail.com>
2024-06-02 17:12:58 +0300

    Initial Commit

---
+++ helloChack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  helloChack
+//
+//  Created by Ярослав  Мартынов on 02.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
