62536dd4a1aa880eb288bcef660f19d9d2f2f431
yarchike <yarchike@gmail.com>
2024-07-16 18:45:20 +0300

    fix

--- helloChack/QuoteManager.swift
+++ helloChack/QuoteManager.swift
@@ -9,18 +9,9 @@ import Foundation
 import RealmSwift
 
 struct QuoteManager {
-    static let encryptionKey: Data = {
-        
-        var keyData = Data(count: 64)
-           _ = keyData.withUnsafeMutableBytes { bytes in
-               SecRandomCopyBytes(kSecRandomDefault, 64, bytes.baseAddress!)
-           }
-    
-        return keyData
-    }()
     
     static func realmInstance() -> Realm {
-        let config = Realm.Configuration(encryptionKey: encryptionKey)
+        let config = Realm.Configuration(encryptionKey: getKey())
         return try! Realm(configuration: config)
     }
 
@@ -67,4 +58,37 @@ struct QuoteManager {
         
         return allQuoteRealm.map({ $0 })
     }
+    
+    static func getKey() -> Data {
+        let keychainIdentifier = "io.Realm.EncryptionExampleKey"
+        let keychainIdentifierData = keychainIdentifier.data(using: String.Encoding.utf8, allowLossyConversion: false)!
+        var query: [NSString: AnyObject] = [
+            kSecClass: kSecClassKey,
+            kSecAttrApplicationTag: keychainIdentifierData as AnyObject,
+            kSecAttrKeySizeInBits: 512 as AnyObject,
+            kSecReturnData: true as AnyObject
+        ]
+    
+        var dataTypeRef: AnyObject?
+        var status = withUnsafeMutablePointer(to: &dataTypeRef) { SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer($0)) }
+        if status == errSecSuccess {
+            return dataTypeRef as! Data
+        }
+        var key = Data(count: 64)
+        key.withUnsafeMutableBytes({ (pointer: UnsafeMutableRawBufferPointer) in
+            let result = SecRandomCopyBytes(kSecRandomDefault, 64, pointer.baseAddress!)
+            assert(result == 0, "Failed to get random bytes")
+        })
+        // Store the key in the keychain
+        query = [
+            kSecClass: kSecClassKey,
+            kSecAttrApplicationTag: keychainIdentifierData as AnyObject,
+            kSecAttrKeySizeInBits: 512 as AnyObject,
+            kSecValueData: key as AnyObject
+        ]
+        status = SecItemAdd(query as CFDictionary, nil)
+        assert(status == errSecSuccess, "Failed to insert the new key in the keychain")
+        return key
+    }
 }
+
